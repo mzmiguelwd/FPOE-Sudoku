44b52dac87535e8d2aaaa6f60001d5577e0a9004
Lexmarten808 <brandon.lasprilla@correounivalle.edu.co>
2025-04-03 08:31:17 -0500

    uptaded main

---
+++ src/main/java/org/example/fpoesudoku/models/AlertHelper.java
@@ -0,0 +1,47 @@
+package org.example.fpoesudoku.models;
+
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import java.util.Optional;
+
+public class AlertHelper {
+
+    // Método para mostrar una alerta de confirmación
+    public static boolean showConfirmationAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+
+        Optional<ButtonType> result = alert.showAndWait();
+        return result.isPresent() && result.get() == ButtonType.OK;
+    }
+
+    // Method to show an alert confirmation
+    public static void showInfoAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }//Used for the start button message confirmation
+
+    // Method to show an error message
+    public static void showErrorAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+    //Method to show a warning message
+    public static void showWarningAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+
+    }
+}
